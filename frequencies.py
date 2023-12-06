79b8c7f8095160d0dc19d13fde05061018825fdf
tanvir-sahota <tanvirsahota07@gmail.com>
2023-12-06 22:15:47 +0000

    done

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,22 @@
 
 def frequencies(items):
     frequencies = {}
+    distinct = []
+
+    for it in items:
+        if isinstance(it, int):
+            it = str(it)
+        if it not in distinct:
+            distinct.append(it)
+
+    for dis in distinct:
+        count = 0
+        for it in items:
+            if isinstance(it, int):
+                it = str(it)
+            if dis == it:
+                count += 1
+        frequencies[dis] = count
+
     # Your code goes here
     return frequencies
